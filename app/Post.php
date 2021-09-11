38d89873ce6fbbe200a9e0870996c21a8d94268e
akmaluddinf <akmaluddinfadhilah@gmail.com>
2021-09-11 10:56:04 +0700

    initial commit

---
+++ app/Post.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Post extends Model
+{
+  // var string
+  protected $table = 'posts';
+
+  // var array
+  protected $fillable = [
+    'title', 'content',
+  ];
+}
